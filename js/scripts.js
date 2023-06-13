95c82fab35f25ae56254ff5bda0c87067f6d100b
Laur <128435654+laurastj@users.noreply.github.com>
2023-06-12 21:10:19 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#banners ul").bxSlider({
+        auto: true,
+        // mode: 'horizontal',
+        // speed: 500,
+        // page: true,
+    });
+    
+    $('#fotos .container').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
